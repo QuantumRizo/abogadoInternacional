f8eb3e0d1a90e7a8ca5a47c5d06e2c892c888936
Felix <davidrizo.phys@gmail.com>
2026-02-13 13:22:34 -0600

    dadawed

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import Index from "./pages/Index";
-import Citas from "./pages/Citas";
 import Admin from "./pages/Admin";
 import NotFound from "./pages/NotFound";
 
